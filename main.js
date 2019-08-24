c328553d68ce5113a51edeb93f479e8a21e3fd57
Caley Woods <caley@vcreativeinc.com>
2019-08-24 15:49:19 -0500

    Begin antipode function

---
+++ main.js
@@ -0,0 +1,5 @@
+const getAntipode = (lat, long, others) => {
+    // @TODO: Flip the North/South of latitude
+    // @TODO: Subtract 180 degrees from the longitude
+    // @TODO: Return a format Google maps can understand
+}
