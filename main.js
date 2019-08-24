8185094575d54130ee04cc8ab060f4b282c383be
Caley Woods <caley@vcreativeinc.com>
2019-08-24 15:51:32 -0500

    Add dom ready function

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const getAntipode = (lat, long, others) => {
     // @TODO: Subtract 180 degrees from the longitude
     // @TODO: Return a format Google maps can understand
 }
+
+const domReady = fn => {
+    document.addEventListener('DOMContentLoaded', fn);
+    const lockedNLoaded = document.readyState === 'interactive' || document.readyState === 'complete';
+
+    if (lockedNLoaded) {
+        fn();
+    }
+}
+
+domReady(_ => {
+    // We're ready
+});
